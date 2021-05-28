13c9486da7e5b0e1ca96066fa4829938584bb2b4
Jonathan Anderfuhren <jonathan.anderfuhren@students.ffhs.ch>
2021-05-28 16:19:54 +0200

    Daily Push

--- lb1_virus_patience_reloaded.py
+++ lb1_virus_patience_reloaded.py
@@ -5,25 +5,32 @@ import subprocess
 
 print("Greetings, this is your friendly neighbourhood virus.")
 print("To stop me, close this terminal before I print the 'proceeding' message.")
-time.sleep(1)
+time.sleep(5)
 print("proceeding")
 
+#get filename and path of current instance
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
-print(filename)
-print(path)
+
+#TODO: Download newest version
+
+#https://github.com/castawaywars/M122_AP18c_Thema_Anderfuhren_Schuhmacher.git
+#lb1_virus_patience_reloaded.py
+#https://github.com/castawaywars/M122_AP18c_Thema_Anderfuhren_Schuhmacher/blob/b2b131f0d197bba3aebfa68e1fb0812fa0004cde/lb1_virus_patience_reloaded.py
+
+#I hope that's the right path. The other three haven't been tested.
+download_path="https://github.com/castawaywars/M122_AP18c_Thema_Anderfuhren_Schuhmacher/blob/Anderfuhren/lb1_virus_patience_reloaded.py"
 
 
+
+#TODO: Store new file in the same directory under a new name
+
+
+
+#prepare filename to be callable
 filestring=str(filename)
-
 filestring='"'+filestring+'"'
-
-print(filestring)
-
+#run new instance
 subprocess.call("start python "+filestring, shell=True)
-
-#os.system(filename)
-
-#exec(open(filename).read())
 
 print("end of instance")
