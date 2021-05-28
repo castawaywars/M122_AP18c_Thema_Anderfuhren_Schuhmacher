57f9cba7f1ee0631c1002ff46b2687125f7af10a
Jonathan Anderfuhren <jonathan.anderfuhren@students.ffhs.ch>
2021-05-28 15:34:24 +0200

    backup commit Here goes nothing

--- lb1_virus_patience_reloaded.py
+++ lb1_virus_patience_reloaded.py
@@ -1,3 +1,20 @@
+import time
+import os
+import inspect
+import subprocess
+
 print("Greetings, this is your friendly neighbourhood virus.")
 print("To stop me, close this terminal before I print the 'proceeding' message.")
-print("aborting")
+time.sleep(1)
+print("proceeding")
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+print(filename)
+print(path)
+
+subprocess.call(filename)
+
+#exec(open(filename).read())
+
+print("end of instance")
