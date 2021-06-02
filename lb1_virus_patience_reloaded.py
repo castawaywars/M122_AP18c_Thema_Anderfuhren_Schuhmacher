2f753bcb40889a8066593368c06eba4128300aba
Jonathan Anderfuhren <jonathan.anderfuhren@students.ffhs.ch>
2021-06-02 12:30:41 +0200

    Successful download, but download unfinished.

--- lb1_virus_patience_reloaded.py
+++ lb1_virus_patience_reloaded.py
@@ -3,14 +3,19 @@ import os
 import inspect
 import subprocess
 
+import urllib.request
+
 print("Greetings, this is your friendly neighbourhood virus.")
 print("To stop me, close this terminal before I print the 'proceeding' message.")
-time.sleep(5)
+time.sleep(1)
 print("proceeding")
 
 #get filename and path of current instance
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
+
+lastindexof=str(filename).rfind('\\')
+filename_only=str(filename)[lastindexof+1:-3]
 
 #TODO: Download newest version
 
@@ -20,17 +25,36 @@ path = os.path.dirname(os.path.abspath(filename))
 
 #I hope that's the right path. The other three haven't been tested.
 download_path="https://github.com/castawaywars/M122_AP18c_Thema_Anderfuhren_Schuhmacher/blob/Anderfuhren/lb1_virus_patience_reloaded.py"
+download_raw="https://raw.githubusercontent.com/castawaywars/M122_AP18c_Thema_Anderfuhren_Schuhmacher/Anderfuhren/lb1_virus_patience_reloaded.py"
+download_raw_auth="https://castawaywars@Rafisa2ProjektCW@raw.github.com/castawaywars/M122_AP18c_Thema_Anderfuhren_Schuhmacher/blob/Anderfuhren/lb1_virus_patience_reloaded.py"
+
+targetfilename=None
+filename_length=len(filename_only)
+if(filename_length!=27):
+	print("triggered")
+else:
+	targetfilename=filename_only+"1.py"
+	print("not triggered")
+
+
+local_filename, headers = urllib.request.urlretrieve(download_raw, filename=targetfilename)
+
+print(local_filename)
+#print(headers)
+
 
 
 
 #TODO: Store new file in the same directory under a new name
-
+#open(path+"test.py").write(req.content)
 
 
 #prepare filename to be callable
 filestring=str(filename)
 filestring='"'+filestring+'"'
 #run new instance
-subprocess.call("start python "+filestring, shell=True)
+#subprocess.call("start python "+filestring, shell=True)
+
+urllib.request.urlcleanup()
 
 print("end of instance")
